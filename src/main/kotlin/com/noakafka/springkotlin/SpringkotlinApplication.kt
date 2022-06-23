eda006dcdeb8ea6eae347b07cd1c1b788e851ef2
NoaKafka <35343777+NoaKafka@users.noreply.github.com>
2022-06-23 23:34:58 +0900

    generate

---
+++ src/main/kotlin/com/noakafka/springkotlin/SpringkotlinApplication.kt
@@ -0,0 +1,11 @@
+package com.noakafka.springkotlin
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SpringkotlinApplication
+
+fun main(args: Array<String>) {
+	runApplication<SpringkotlinApplication>(*args)
+}
